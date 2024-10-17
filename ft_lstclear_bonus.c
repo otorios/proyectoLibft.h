25299e4368a5ae3a86a7b7d3213a6a03ede05ae1
otorios <sanchezolalla24gmail.com>
2024-10-17 19:27:52 +0200

    ft_lstclear terminado

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,56 @@
 /*   By: olalsanc <olalsanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 16:00:27 by olalsanc          #+#    #+#             */
-/*   Updated: 2024/10/16 18:41:39 by olalsanc         ###   ########.fr       */
+/*   Updated: 2024/10/17 19:27:21 by olalsanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+// Función que libera el contenido del nodo (en este caso, un entero)
+void	del(void *content)
+{
+	free (content);
+}
+
+// Función que libera los nodos de una lista enlazada
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	if (!lst || !*lst || !del)
+		return ;
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		del((*lst)->content);
+		free(*lst);
+		*lst = temp;
+	}
+	*lst = NULL;
+}
+/* 
+#include <stdio.h>
+int main()
+{
+    t_list *head = ft_lstnew(malloc(sizeof(int))); // Crear el primer nodo
+    if (!head) return 1; // Verificar error de malloc
+    *(int *)(head->content) = 1; // Asignar contenido al primer nodo
+
+    head->next = ft_lstnew(malloc(sizeof(int))); // Crear segundo nodo
+    if (!head->next) return 1; // Verificar error de malloc
+    *(int *)(head->next->content) = 2; // Asignar contenido al segundo nodo
+
+    head->next->next = ft_lstnew(malloc(sizeof(int))); // Crear tercer nodo
+    if (!head->next->next) return 1; // Verificar error de malloc
+    *(int *)(head->next->next->content) = 3; // Asignar contenido al tercer nodo
+
+    // Llamar a la función para limpiar la lista
+    ft_lstclear(&head, del); 
+
+    // Comprobar si la lista está vacía
+    if (head == NULL)
+        printf("La lista ha sido limpiada y está vacía.\n");
+
+    return (0); // Fin del programa
+} */
--- libft.h
+++ libft.h
@@ -6,11 +6,11 @@
 /*   By: olalsanc <olalsanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 16:25:37 by olalsanc          #+#    #+#             */
-/*   Updated: 2024/10/17 18:27:41 by olalsanc         ###   ########.fr       */
+/*   Updated: 2024/10/17 19:27:35 by olalsanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H   // Si LIBFT_H no está definido...
+#ifndef LIBFT_H   // if LIBFT_H no está definido...
 # define LIBFT_H   // Defínelo para evitar inclusiones múltiples
 
 #include <stddef.h>
@@ -26,22 +26,23 @@ typedef struct s_list
     struct s_list *next;
 } t_list;
 
-t_list *ft_lstnew(void *content);
+t_list  *ft_lstnew(void *content);
 void	ft_lstadd_front(t_list	**lst, t_list	*new);
-int	ft_lstsize(t_list	*lst);
+int	    ft_lstsize(t_list	*lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 //----------------1º part---------------------
 
-int ft_isascii(int x);
-int	ft_toupper(char x);
-int	ft_tolower(char x);
-int	ft_isdigit(char number);
-int	ft_isalpha(char str);
-int ft_isalnum(char x);
-int	ft_isprint(int x);
+int     ft_isascii(int x);
+int     ft_toupper(char x);
+int	    ft_tolower(char x);
+int	    ft_isdigit(char number);
+int	    ft_isalpha(char str);
+int     ft_isalnum(char x);
+int	    ft_isprint(int x);
 size_t	ft_strlen(const char	*str);
 void	ft_bzero(void *s, size_t n);
 //void ft_memset(char *x, int value, size_t varsize, size_t buffer_size);
@@ -49,14 +50,14 @@ void    *ft_memcpy(const void	*source, void	*dest, size_t	num);
 //int ft_strchr(char *str, char x);
 //char *ft_strrchr(char *str, char x);
 //char *ft_memchr (const char    *str, int  byte_x, size_t   byte_lenght);
-int	ft_memcmp(const void	*str1, const void	*str2, size_t	byte_size);
+int	    ft_memcmp(const void	*str1, const void	*str2, size_t	byte_size);
 void	*ft_calloc(size_t	num_elements, size_t	element_size);
 char	*ft_strdup(const char	*s);
 
 //----------------2º part---------------------
 
 void	ft_putchar_fd(char c, int fd);
-void ft_putstr_fd(char *s, int fd);
+void    ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 char	*ft_itoa(int n);
