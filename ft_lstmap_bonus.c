d2ba3beed9e015c1149237aa17a7e752477298e6
otorios <sanchezolalla24gmail.com>
2024-10-19 13:11:46 +0200

    f_lstmap termianda

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,124 +6,87 @@
 /*   By: olalsanc <olalsanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 16:01:33 by olalsanc          #+#    #+#             */
-/*   Updated: 2024/10/18 18:09:56 by olalsanc         ###   ########.fr       */
+/*   Updated: 2024/10/19 13:03:10 by olalsanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//gcc -Wall -Wextra -Werror ft_lstmap_bonus.c ft_lstnew_bonus.c ft_lstclear_bonus.c ft_lstadd_back_bonus.c ft_strlcpy.c ft_strlen
 
-
-/* void	*f(void	*content)
+void	*add_dash(void	*content)
 {
 	char	*str;
+	char	*new_str;
 
 	str = (char *)content;
-	if (str)
-		str[0] = '-';
-	return (str);
-} */
-void *f(void *content)
-{
-    char *str = (char *)content;
-    if (!str)
-        return NULL;
-
-    // Crea una nueva cadena y copia el contenido
-    char *new_str = malloc(ft_strlen(str) + 1); // Reserva memoria
-    if (!new_str)
-        return NULL; // Verifica la reserva
-
-    ft_strlcpy(new_str, str, 3); // Copia el contenido
-    new_str[0] = '-'; // Cambia el primer carácter
-
-    return new_str; // Devuelve la nueva cadena
+	new_str = malloc(ft_strlen(str) + 2);
+	if (new_str)
+	{
+		new_str[0] = '-';
+		ft_strlcpy(new_str + 1, str, ft_strlen(str) + 1);
+	}
+	return (new_str);
 }
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+void	del_content(void *content)
 {
-    t_list *new_lst;  // Puntero a la nueva lista
-    t_list *new_node; // Puntero para crear cada nuevo nodo
+	free(content);
+}
 
-    if (!lst || !f)  // Verifica que la lista y la función no sean nulos
-        return (NULL);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*next_node;
+	void	*new_content;
+	t_list	*new_node;
 
-    new_lst = NULL;  // Inicializa la nueva lista en NULL
+	if (!lst)
+		return (NULL);
+	next_node = ft_lstmap(lst->next, f, del);
+	new_content = f(lst->content);
+	new_node = ft_lstnew(new_content);
+	if (!new_node)
+	{
+		del(new_content);
+		return (NULL);
+	}
+	new_node->next = next_node;
+	return (new_node);
+}
+/* #include <stdio.h>
+//gcc -Wall -Wextra -Werror ft_lstmap_bonus.c ft_strlen.c ft_lstnew_bonus.c 
+//ft_lstclear_bonus.c ft_strdup.c ft_memcpy.c ft_strlcpy.c 
+int main()
+{
+    // Creamos una lista simple
+    t_list *original = ft_lstnew(ft_strdup("Hola"));
+	// Usamos strdup para duplicar cadenas
+    original->next = ft_lstnew(ft_strdup("Mundo"));
+    original->next->next = ft_lstnew(ft_strdup("!"));
 
-    // Mientras queden nodos en la lista original
-    while (lst)
+    // Imprimimos la lista original
+    printf("Lista original:\n");
+    t_list *temp = original;
+    while (temp)
     {
-        // Aplica la función 'f' al contenido del nodo actual y crea un nuevo nodo con ese contenido
-        new_node = ft_lstnew(f(lst->content));
-
-        // Si falla la creación del nuevo nodo, libera toda la memoria asignada hasta el momento
-        if (!new_node)
-        {
-            ft_lstclear(&new_lst, del);  // Libera la memoria de la nueva lista
-            return (NULL);
-        }
-
-        // Añade el nuevo nodo a la lista nueva
-        ft_lstadd_back(&new_lst, new_node);
-
-        // Avanza al siguiente nodo en la lista original
-        lst = lst->next;
+        printf("%s\n", (char *)temp->content);
+        temp = temp->next;
     }
 
-    // Devuelve la nueva lista
-    return (new_lst);
-}
-#include <stdio.h>
+    // Aplicamos ft_lstmap
+    t_list *modified = ft_lstmap(original, add_dash, del_content);
 
-void *duplicate_content(void *content)
-{
-    int *new_content = malloc(sizeof(int));
-    if (!new_content)
-        return (NULL);
-    *new_content = *(int *)content;  // Solo copia el valor, no lo duplica
-    return (new_content);
-}
+    // Imprimimos la lista modificada
+    printf("\nLista modificada:\n");
+    temp = modified;
+    while (temp)
+    {
+        printf("%s\n", (char *)temp->content);
+        temp = temp->next;
+    }
 
-
-// Función para liberar el contenido (simple para enteros)
-void del_content(void *content)
-{
-    free(content);
-}
-
-int main(void)
-{
-    t_list *lst = ft_lstnew(malloc(sizeof(int)));
-    t_list *new_lst;
-    *(int *)(lst->content) = 42; // Asignar un valor al contenido
-
-    // Agregar más nodos a la lista original
-    ft_lstadd_back(&lst, ft_lstnew(malloc(sizeof(int))));
-    *(int *)(lst->next->content) = 21;
-
-    // Llamar a ft_lstmap con la función que duplica el contenido
-    new_lst = ft_lstmap(lst, duplicate_content, del_content);
-
-    // Imprimir los valores de la nueva lista
-
-	printf("Nueva lista:\n");
-	while (new_lst)
-	{
-		printf("%d\n", *(int *)(new_lst->content));
-		new_lst = new_lst->next;
-	}
-
-	// Imprimir los valores de la lista original
-	printf("Lista original:\n");
-	while (lst)
-	{
-		printf("%d\n", *(int *)(lst->content));
-		lst = lst->next;
-	}
-
-    // Liberar memoria
-    ft_lstclear(&lst, del_content);
-    ft_lstclear(&new_lst, del_content);
+    // Liberamos memoria
+    ft_lstclear(&original, del_content); // Liberamos la lista original
+    ft_lstclear(&modified, del_content); // Liberamos la lista modificada
 
     return (0);
 }
+ */
--- libft.h
+++ libft.h
@@ -6,73 +6,76 @@
 /*   By: olalsanc <olalsanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 16:25:37 by olalsanc          #+#    #+#             */
-/*   Updated: 2024/10/19 11:29:23 by olalsanc         ###   ########.fr       */
+/*   Updated: 2024/10/19 13:10:58 by olalsanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H   // if LIBFT_H no está definido...
-# define LIBFT_H   // Defínelo para evitar inclusiones múltiples
-
-#include <stddef.h>
-#include <stdlib.h>
-#include <unistd.h>
+#ifndef LIBFT_H // if LIBFT_H no está definido...
+# define LIBFT_H // Defínelo para evitar inclusiones múltiples
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 // Aquí van las declaraciones de las funciones
 //----------------Bonus part---------------------
 
 typedef struct s_list
 {
-    void *content;
-    struct s_list *next;
-} t_list;
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
-t_list  *ft_lstnew(void *content);
-void	ft_lstadd_front(t_list	**lst, t_list	*new);
-int	    ft_lstsize(t_list	*lst);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void *));
-void	ft_lstclear(t_list **lst, void (*del)(void *));
-void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstnew(void *content);
+void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
 
 //----------------1º part---------------------
 
-int     ft_isascii(int x);
-int     ft_toupper(char x);
-int	    ft_tolower(char x);
-int	    ft_isdigit(char number);
-int	    ft_isalpha(char str);
-int     ft_isalnum(char x);
-int	    ft_isprint(int x);
-size_t	ft_strlen(const char	*str);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memset(void	*s, int c, size_t	n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-char    *strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-void	*ft_memchr(const void *s, int c, size_t n);
-int	    ft_memcmp(const void	*str1, const void	*str2, size_t	byte_size);
-int	    ft_strncmp(const char	*s1, const char *s2, size_t n);
-int	    ft_atoi(const char *nptr);
-void	*ft_memmove(void	*dest, const void	*src, size_t	n);
-size_t	ft_strlcpy(char	*dst, const char *src, size_t size);
-size_t	ft_strlcat(char *dest, const char *src, size_t size);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
-void	*ft_calloc(size_t	num_elements, size_t	element_size);
-char	*ft_strdup(const char	*s);
+int					ft_isascii(int x);
+int					ft_toupper(char x);
+int					ft_tolower(char x);
+int					ft_isdigit(char number);
+int					ft_isalpha(char str);
+int					ft_isalnum(char x);
+int					ft_isprint(int x);
+size_t				ft_strlen(const char *str);
+void				ft_bzero(void *s, size_t n);
+void				*ft_memset(void *s, int c, size_t n);
+void				*ft_memcpy(void *dest, const void *src, size_t n);
+char				*strchr(const char *s, int c);
+char				*ft_strrchr(const char *s, int c);
+void				*ft_memchr(const void *s, int c, size_t n);
+int					ft_memcmp(const void *str1, const void *str2,
+						size_t byte_size);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+int					ft_atoi(const char *nptr);
+void				*ft_memmove(void *dest, const void *src, size_t n);
+size_t				ft_strlcpy(char *dst, const char *src, size_t size);
+size_t				ft_strlcat(char *dest, const char *src, size_t size);
+char				*ft_strnstr(const char *big, const char *little,
+						size_t len);
+void				*ft_calloc(size_t num_elements, size_t element_size);
+char				*ft_strdup(const char *s);
 
 //----------------2º part---------------------
 
-void	ft_putchar_fd(char c, int fd);
-void    ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-char	*ft_itoa(int n);
-char	*ft_substr(char const	*s, unsigned int start, size_t	len);
-char	*ft_strtrim(char const	*s1, char const	*set);
-void	ft_striteri(char	*s, void (*f)(unsigned int, char*));
-char	*ft_strmapi(char const	*s, char (*f)(unsigned int, char));
-char	*ft_strjoin(char const *s1, char const *s2);
-char	**ft_split(char const	*s, char c);
+void				ft_putchar_fd(char c, int fd);
+void				ft_putstr_fd(char *s, int fd);
+void				ft_putendl_fd(char *s, int fd);
+void				ft_putnbr_fd(int n, int fd);
+char				*ft_itoa(int n);
+char				*ft_substr(char const *s, unsigned int start, size_t len);
+char				*ft_strtrim(char const *s1, char const *set);
+void				ft_striteri(char *s, void (*f)(unsigned int, char *));
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char				*ft_strjoin(char const *s1, char const *s2);
+char				**ft_split(char const *s, char c);
 
 #endif
