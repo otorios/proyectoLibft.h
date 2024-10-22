8520df757c4f3e8263c899cbc0756b24fc827b26
otorios <sanchezolalla24gmail.com>
2024-10-22 17:35:43 +0200

    ft_memcpy sin control de NULL

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: olalsanc <olalsanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 15:53:23 by olalsanc          #+#    #+#             */
-/*   Updated: 2024/10/21 18:52:37 by olalsanc         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:35:19 by olalsanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	const unsigned char	*sour;
 	unsigned char		*des;
 
-	if (!src || !dest)
-		return (NULL);
 	sour = (const unsigned char *) src;
 	des = (unsigned char *) dest;
 	while (n--)
@@ -29,14 +27,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 int	main(void)
 {
 	printf("\nTest de ft_memcpy()\n");
-	
-	//Cuando trabajas con cadenas de caracteres en C y utilizas funciones
-	//como printfcon el formato %s, el formato %s está diseñado específicamente
-	//para trabajar con cadenas de caracteres (arrays de char) que están 
-	//terminadas en un carácter nulo ('\0')
-	//La función printf reconoce automáticamente el carácter nulo para saber 
-	//dónde termina la cadena y por lo tanto, no necesitas un bucle explícito 
-	//para imprimir cada carácter.
 	
 	char	source[6] = "Mundo";
 	char	dest[6] = "Hola";
@@ -61,12 +51,6 @@ int	main(void)
 	int source_ints[5] = {1, 2, 3, 4, 5}; // Array de enteros de origen
 	int dest_ints[5] = {0}; // Array de enteros de destino, inicializado a 0
 	size_t num_ints = 5 * sizeof(int); 
-	//Número de bytes a copiar (5 enteros);
-	//El valor 5 que estás pasando a ft_memcpy solo indica el número de enteros,
-	//no el número total de bytes. 
-	//Dado que cada entero puede ser de 4 bytes en la mayoría de las plataformas,
-	//estás copiando solo 5 bytes, lo cual no es suficiente para copiar todos 
-	//los enteros.
 
 	printf("Valor de dest_ints antes de concatenar: ");
 	i = 0;
@@ -84,7 +68,10 @@ int	main(void)
 		printf("%d ", dest_ints[i]);
 		i++;
 	}
-    	printf("\n");
-    	return (0);
-}
- */
+    printf("\n");
+	
+	printf("Pasando null:\n");
+	ft_memcpy(NULL, "Mundo", 5);
+
+    return (0);
+} */
